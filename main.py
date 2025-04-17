ec2c492d60e3cf16486884678cfd53be5006e552
Ananth <64283902+itsAnanth@users.noreply.github.com>
2025-04-17 18:40:05 +0530

    chore: readability and cleanup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 df=pd.read_csv("data.csv")
 
+# activation function
 def sigmoid(z):
     return 1/(1+np.exp(-z))
 
@@ -32,20 +33,24 @@ for index, row in df.iterrows():
     numerator += (xi - x_mean) * (yi - y_mean)
     denominator += (xi - x_mean) ** 2
 
+# Linear regression is of the form y = b0 + b1x
+# general solution is given by (X'.X)^-1.X'y
+# calculated b0 and b1
+
 b1 = numerator / denominator
 b0 = y_mean - (b1 * x_mean)
 
+# calculate probability for every row based on determined values of b0 and b1
 for i in range(len(df)):
     xi = df.loc[i, 'Medication Dosage']
     y = sigmoid(b1 * xi + b0)
     print("prob", y)
 
     df.loc[i, 'predicted_probability'] = y
-    if (y > 0.5):
-        print("cured")
-    else:
-        print("Not cured")
+    print("cured" if y > 0.5 else "not cured")
 
+
+# plot decision boundary
 plt.figure(figsize=(10, 6))
 plt.scatter(df['Medication Dosage'], df['predicted_probability'], color='blue', label='Predicted Probability')
 plt.axhline(0.5, color='red', linestyle='--', label='Threshold (0.5)')
